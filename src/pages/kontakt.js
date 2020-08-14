157b04e566356d9c5bf55975e720ee51d570c5a1
Piotr Krasny <krasny.piotr@gmail.com>
2020-08-14 17:38:28 +0200

    Add contact form and deploy to check build version

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import HeadingSubpage from "../components/Sections/HeadingSubpage/HeadingSubpage"
-// import ContactForm from "../components/ContactForm/ContactForm"
+import ContactForm from "../components/ContactForm/ContactForm"
 
 const KontaktPage = ({ data }) => (
   <>
@@ -14,7 +14,7 @@ const KontaktPage = ({ data }) => (
     />
     <p>asas</p>
     <p>asas</p>
-    {/* <ContactForm></ContactForm> */}
+    <ContactForm></ContactForm>
   </>
 )
 
